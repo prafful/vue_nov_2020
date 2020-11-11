b08ec108dadca98b02729ac0438dce32e10dc1a2
prafful <prraful@gmail.com>
2020-11-10 23:19:23 -0800

    props

--- mythirdvue_spa/src/components/Home.vue
+++ mythirdvue_spa/src/components/Home.vue
@@ -1,14 +1,46 @@
 <template>
     <div>
         <h1>{{message}}</h1>
+        <appNews 
+            newsitem="Today is day 06 with Vue!"
+            v-bind:somedetails="details">
+        </appNews>
     </div>
 </template>
 
 <script>
+import News from './News'
+
 export default {
+    components:{
+        appNews: News
+    },
     data: function(){
         return {
-            message: "Home"
+            message: "Home",
+            details:[
+                {
+                    id:1,
+                    name:'OBB',
+                    location:'Chennai'
+                },
+                {
+                    id:2,
+                    name: "CAS",
+                    location:"Chennai"
+                },
+                {
+                    id:3,
+                    name:"OWIOH",
+                    location:"Japan"
+                },
+                {
+                    id:4,
+                    name:"BNP",
+                    location:"Mumbai"
+                }
+
+        ]
         }
     }
 }
---
+++ mythirdvue_spa/src/components/News.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>{{message}}</h1>
+        <p>{{newsitem}}</p>
+        <ol>
+            <li v-for="detail in somedetails" v-bind:key="detail.id">
+                {{detail.name}}
+            </li>
+        </ol>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        newsitem: String,
+        somedetails: Array
+    },
+    data: function(){
+        return {
+            message: "News"
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
